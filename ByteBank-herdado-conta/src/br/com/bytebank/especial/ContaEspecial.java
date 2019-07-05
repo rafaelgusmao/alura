910f6ab945b19c79bc2d1747dc6ac4dc3ad5c394
c101597 <c101597@DF7390NB338.corp.caixa.gov.br>
2019-07-05 10:35:47 -0300

    Commit do package especial

---
+++ ByteBank-herdado-conta/src/br/com/bytebank/especial/ContaEspecial.java
@@ -0,0 +1,18 @@
+package br.com.bytebank.especial;
+
+import br.com.bytebank.modelo.Conta;
+
+public class ContaEspecial extends Conta {
+
+	public ContaEspecial(int agencia, int numero) {
+		super(agencia, numero);
+
+	}
+	
+	@Override
+	public void deposita(double valor) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
